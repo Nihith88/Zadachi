6642fe45aae2fbea68e6d6e102aa48d5a9df2d0e
Nihith88 <nihith88@yandex.ru>
2022-01-28 16:27:43 +0300

    one-liners

--- arifmetika.py
+++ arifmetika.py
@@ -13,25 +13,19 @@ for i in spisok:
 
 st_num1 = ''.join(spisok[:border])
 st_num2 = ''.join(spisok[border + 1:])
-
-if '.' not in st_num1:
-    srez1 = int(st_num1)
-else:
-    srez1 = float(st_num1)
-
-if '.' not in st_num2:
-    srez2 = int(st_num2)
-else:
-    srez2 = float(st_num2)
-
-if actn == '+':
-    print(srez1 + srez2)
-elif actn == '-':
-    print(srez1 - srez2)
-elif actn == '*':
-    print(srez1 * srez2)
-elif actn == '/':
-    try:
-        print(srez1 / srez2)
-    except ZeroDivisionError:
-        print('Деление на ноль')
+try:
+    srez1 = (int(st_num1) if '.' not in st_num1 else float(st_num1))
+    srez2 = (int(st_num2) if '.' not in st_num2 else float(st_num2))
+    if actn == '+':
+        print(srez1 + srez2)
+    elif actn == '-':
+        print(srez1 - srez2)
+    elif actn == '*':
+        print(srez1 * srez2)
+    elif actn == '/':
+        try:
+            print(srez1 / srez2)
+        except ZeroDivisionError:
+            print('Деление на ноль')
+except ValueError:
+    print('ошибка ввода')
